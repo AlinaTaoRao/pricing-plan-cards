34d21a5719082a033c66dd8b064fd5b7357775eb
AlinaTaoRao <raotao77@outlook.com>
2022-04-22 12:29:31 +0200

    create cards data

--- pricing-plan/src/App.js
+++ pricing-plan/src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { Container } from "./components/Container.jsx";
 import { Card } from "./Card.jsx"; // for way 3
 
-
 /* for way 1 & 2 {} */
 // function App() {
 //   return (
@@ -16,14 +15,58 @@ import { Card } from "./Card.jsx"; // for way 3
 
 /* way 3: generate cards */
 const cards = [
-  { divClass: "card-1", h3: "Free", p: "For Individual", h2: "$0" },
-  { divClass: "card-2", h3: "Pro", p: "For Company", h2: "$80" },
-  { divClass: "card-3", h3: "Enterprise", p: "For Team", h2: "$180" },
+  {
+    cardClass: "card-1",
+    cardText: {
+      h3: "Free",
+      p: "For Individual",
+      h2: "$0",
+    },
+    iconData: {
+      iconClass: "icon1",
+      i: "gg-check-o",
+      h4: ["One Device", "Unlimited Download", "Editables Files", "1GB Stoage"],
+    },
+    btn: { btnClass: "submit1", btn: "Subscribe" },
+  },
+  {
+    cardClass: "card-2",
+    cardText: {
+      h3: "Pro",
+      p: "For Company",
+      h2: "$80",
+    },
+    iconData: {
+      iconClass: "icon1",
+      i: "gg-check-o",
+      h4: ["6 Device", "Unlimited Download", "300+ icon Package", "Fully Editables Files", "500GB Stoage"],
+    },
+    btn: { btnClass: "submit2", btn: "Subscribe" },
+  },
+  {
+    cardClass: "card-3",
+    cardText: {
+      h3: "Enterprise",
+      p: "For Team",
+      h2: "$180",
+    },
+    iconData: {
+      iconClass: "icon1",
+      i: "gg-check-o",
+      h4: ["12 Device", "Unlimited Download", "Fully Editables Files", "2TB Stoage", "5TB Data Transfer"],
+    },
+    btn: { btnClass: "submit3", btn: "Subscribe" },
+  },
+
+  // { divClass: "card-2", h3: "Pro", p: "For Company", h2: "$80" },
+  // { divClass: "card-3", h3: "Enterprise", p: "For Team", h2: "$180" },
 ];
 function App() {
   return (
     <div className="container">
-      {cards.map((card, index) => (<Card key = {index} className={card.divClass} h3 ={card.h3} h2 = {card.h2}/>))}
+      {cards.map((card, index) => (
+        <Card key={index} className={card.divClass} h3={card.h3} h2={card.h2} />
+      ))}
     </div>
   );
 }
