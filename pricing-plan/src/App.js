4d732e01d4ba709e77ba55ab020993e57bd83379
AlinaTaoRao <raotao77@outlook.com>
2022-04-22 15:40:12 +0200

    add way 3: generate cards

--- pricing-plan/src/App.js
+++ pricing-plan/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
-import { Container } from "./components/Container.jsx";
+// import { Container } from "./components/Container.jsx"; // for way 2 and 2 {}
 import { Card } from "./Card.jsx"; // for way 3
 
-/* for way 1 & 2 {} */
+/* for way 1 & 2 {} , works*/
 // function App() {
 //   return (
 //     <>
@@ -16,56 +16,57 @@ import { Card } from "./Card.jsx"; // for way 3
 /* way 3: generate cards */
 const cards = [
   {
-    cardClass: "card-1",
-    cardText: {
-      h3: "Free",
-      p: "For Individual",
-      h2: "$0",
-    },
-    iconData: {
-      iconClass: "icon1",
-      i: "gg-check-o",
-      h4: ["One Device", "Unlimited Download", "Editables Files", "1GB Stoage"],
-    },
-    btn: { btnClass: "submit1", btn: "Subscribe" },
+    cardText: [
+      { h3: "Free" },
+      { p: "For Individual" },
+      { h2: "$0" },
+      { btn: "Subscribe" },
+    ],
+    h4: ["One Device", "Unlimited Download", "Editables Files", "1GB Stoage"],
   },
   {
-    cardClass: "card-2",
-    cardText: {
-      h3: "Pro",
-      p: "For Company",
-      h2: "$80",
-    },
-    iconData: {
-      iconClass: "icon1",
-      i: "gg-check-o",
-      h4: ["6 Device", "Unlimited Download", "300+ icon Package", "Fully Editables Files", "500GB Stoage"],
-    },
-    btn: { btnClass: "submit2", btn: "Subscribe" },
+    cardText: [
+      { h3: "Pro" },
+      { p: "For Company" },
+      { h2: "$80" },
+      { btn: "Subscribe" },
+    ],
+    h4: [
+      "6 Device",
+      "Unlimited Download",
+      "300+ icon Package",
+      "Fully Editables Files",
+      "500GB Stoage",
+    ],
   },
   {
-    cardClass: "card-3",
-    cardText: {
-      h3: "Enterprise",
-      p: "For Team",
-      h2: "$180",
-    },
-    iconData: {
-      iconClass: "icon1",
-      i: "gg-check-o",
-      h4: ["12 Device", "Unlimited Download", "Fully Editables Files", "2TB Stoage", "5TB Data Transfer"],
-    },
-    btn: { btnClass: "submit3", btn: "Subscribe" },
-  },
+    cardText: [
+      { h3: "Enterprise" },
+      { p: "For Team" },
+      { h2: "$180" },
+      { btn: "Subscribe" },
+    ],
 
-  // { divClass: "card-2", h3: "Pro", p: "For Company", h2: "$80" },
-  // { divClass: "card-3", h3: "Enterprise", p: "For Team", h2: "$180" },
+    h4: [
+      "12 Device",
+      "Unlimited Download",
+      "Fully Editables Files",
+      "2TB Stoage",
+      "5TB Data Transfer",
+    ],
+  },
 ];
 function App() {
   return (
     <div className="container">
       {cards.map((card, index) => (
-        <Card key={index} className={card.divClass} h3={card.h3} h2={card.h2} />
+        <Card
+          key={index}
+          cardClass={`card-${index + 1}`}
+          cardText={card.cardText}
+          h4={card.h4}
+          btnClass={`submit${index + 1}`}
+        />
       ))}
     </div>
   );
--- pricing-plan/src/Card.jsx
+++ pricing-plan/src/Card.jsx
@@ -1,9 +1,23 @@
-export const Card = ({ className, h3, h2 }) => {
+/* for way 3: generate cards */
+import './Card.css';
+
+export const Card = ({ cardClass, cardText, h4, btnClass}) => {
+  console.log("cardText:", cardText);
+  console.log("h4:", h4);
+  console.log("btnClass:", btnClass);
   return (
-    <div class={className}>
-      <h3>{h3}</h3>
-      <p>For Individual</p>
-      <h2>{h2}</h2>
+    <div className={cardClass}>
+      <h3>{cardText[0].h3}</h3>
+      <p>{cardText[1].p}</p>
+      <h2>{cardText[2].h2}</h2>
+      {h4.map((item, index) => (
+        <div key={index} className="icon1">
+          <i className="gg-check-o"></i>
+          <h4>{item}</h4>
+        </div>
+      ))}
+      <button className={btnClass}>{cardText[3].btn}</button>
     </div>
   );
 };
+
